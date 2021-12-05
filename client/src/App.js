5f57ca7bccd94b6c851261a460277cdea5bfcd70
Ruan de Klerk <Ruan@Ruans-iMac.lan>
2021-12-05 16:02:06 +1300

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Rentals from './pages/Rentals';
+
+function App() {
+  return (
+    <div className="App">
+     <Rentals />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/pages/Rentals.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import GoogleMaps from '../components/GoogleMaps';
+
+
+function Rentals() {
+
+
+    return (
+        <GoogleMaps />
+    )
+}
+
+export default Rentals;
